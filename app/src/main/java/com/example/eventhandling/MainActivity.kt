82d43ae7195708c9bb8f7ec28ad72682a3bd8bea
AndroidExtension <mhddicky171001@gmail.com>
2022-04-23 10:13:50 +0700

    EventHandling

---
+++ app/src/main/java/com/example/eventhandling/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.eventhandling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnDisplayMessage.setOnClickListener(View.OnClickListener { Toast.makeText(applicationContext, "Hello World", Toast.LENGTH_LONG).show() })
+        btnDisplayMessage.setOnLongClickListener(View.OnLongClickListener { true })
+    }
+}
